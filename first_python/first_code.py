0de1023201108ea122c7f651dd17f69284a7998f
Andrew5c <1141936550@qq.com>
2020-05-25 12:45:11 +0800

    note for first python

---
+++ first_python/first_code.py
@@ -0,0 +1,12 @@
+# method 1
+print("hello world!")
+
+# method 2
+print('hello', 'world')
+
+print(300)
+
+print('100+100 =', 100+100)
+
+name = input('please input your name: ')
+print('hello', name)
